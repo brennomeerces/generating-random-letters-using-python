c8778d829602b2281039627aec1242b3807bc264
Brenno Mercês <93288911+brennomeerces@users.noreply.github.com>
2021-12-18 02:32:43 -0300

    Add files via upload

---
+++ Geração de Letras em sequências aleatórias possuindo ao menos duas vogais. .py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+
+import random as rd 
+
+#Realizar a sequência aleatória através da biblioteca random 
+
+randlowercase = chr(rd.randint(ord('A'), ord('Z')))
+randuppercase = chr(rd.randint(ord('B'), ord('Z')))
+randuppercase1 = chr(rd.randint(ord('C'), ord('Z')))
+randuppercase2 = chr(rd.randint(ord('A'), ord('Z')))
+randuppercase3 = chr(rd.randint(ord('D'), ord('Z')))
+randuppercase4 = chr(rd.randint(ord('A'), ord('A')))
+randuppercase5 = chr(rd.randint(ord('F'), ord('Z')))
+randuppercase6 = chr(rd.randint(ord('G'), ord('Z')))
+randuppercase7 = chr(rd.randint(ord('A'), ord('Z')))
+randuppercase8 = chr(rd.randint(ord('E'), ord('E')))
+randuppercase9 = chr(rd.randint(ord('A'), ord('Z')))
+
+print(randlowercase, randuppercase, randuppercase1, randuppercase2, randuppercase3, randuppercase4, randuppercase5, randuppercase6, randuppercase7, randuppercase8, randuppercase9)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
